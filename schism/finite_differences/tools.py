Staggering: added parsing of NODE and correct usage of derivative x0

schism/finite_differences/tools.py
@@ -4,6 +4,7 @@ import sympy as sp
 import numpy as np
 
 from functools import reduce
+import devito as dv
 
 
 def get_sten_vector(deriv, points):
@@ -21,6 +22,17 @@ def get_sten_vector(deriv, points):
     # Get the indices
     inds = np.arange(-s_o//2, s_o//2 + 1)
 
+    # Account for the function staggering
+    # FIXME: The dv.NODE thing here might not work
+    if func.staggered == dv.NODE or func.staggered is None:
+        stagger = {dim: 0 for dim in func.space_dimensions}
+    elif isinstance(func.staggered, dv.Function):
+        stagger = {dim: (dim.spacing/2 if dim == func.staggered else 0)
+                   for dim in func.space_dimensions}
+    else:
+        stagger = {dim: (dim.spacing/2 if dim in func.staggered else 0)
+                   for dim in func.space_dimensions}
+
     dim_coeffs = []  # List of coefficients per dimension
     for dim in func.space_dimensions:
         try:
@@ -29,7 +41,8 @@ def get_sten_vector(deriv, points):
             order = 0
         try:
             # Get the relative offset (set dim to zero and spacing to 1)
-            x0 = float(deriv.x0[dim].subs([(dim, 0), (dim.spacing, 1)]))
+            x0 = float((deriv.x0[dim]
+                        - stagger[dim]).subs([(dim, 0), (dim.spacing, 1)]))
         except KeyError:
             x0 = 0
         dim_coeffs.append(np.array(sp.finite_diff_weights(order,