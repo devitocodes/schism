ce0b6b276263ba254815faa78bc9921f93fa5a24
EdCaunt <ec815@ic.ac.uk>
2022-12-14 12:52:15 +0000

    Examples: started work on 3D free surface example

--- examples/2nd_order/3D_free_surface.py
+++ examples/2nd_order/3D_free_surface.py
@@ -1,5 +1,5 @@
 """
-Simple example implementing rigid-surface with the 2nd-order acoustic wave
+Simple 3D example implementing free-surface with the 2nd-order acoustic wave
 equation. This code will save four snapshots at intervals of 0.2*tn, where tn
 is the end time (zero timestep will not be outputted). This code will use
 real-world topography taken from a Digital Elevation Map (DEM) of Mt St
@@ -25,36 +25,36 @@ def run(sdf, s_o, nsnaps):
     # Create pressure function
     p = dv.TimeFunction(name='p', grid=grid, space_order=s_o, time_order=2)
 
-    bc_list = [dv.Eq(p.dx, 0),
-               dv.Eq(p.dy, 0)]
+    bc_list = [dv.Eq(p, 0),  # Zero pressure on free surface
+               dv.Eq(p.dx2+p.dy2+p.dy2, 0)]  # Zero laplacian
 
     if s_o >= 4:
-        bc_list += [dv.Eq(p.dx3 + p.dxdy2, 0),
-                    dv.Eq(p.dx2dy + p.dy3, 0)]
+        bc_list.append(dv.Eq(p.dx4 + p.dy4 + p.dz4
+                             + 2*p.dx2dy2 + 2*p.dx2dz2 + 2*p.dy2dz2, 0))
 
     # TODO: add higher-order bcs
     bcs = BoundaryConditions(bc_list)
     boundary = Boundary(bcs, bg)
 
-    derivs = (p.dx2, p.dy2)
+    derivs = (p.dx2, p.dy2, p.dz2)
     subs = boundary.substitutions(derivs)
 
-    # FIXME: Adjust all these accordingly to scale the model
-    c = 0.35  # km/s
+    c = 2.5  # km/s
 
     t0 = 0.  # Simulation starts a t=0
-    tn = 10000.  # Simulation last 10 seconds (10000 ms)
+    tn = 1500.  # Simulation last 0.8 seconds (800 ms)
     # Note: grid increment hardcoded, courant number 0.5
     dt = 0.5*30/c  # Time step from grid spacing
 
     time_range = TimeAxis(start=t0, stop=tn, step=dt)
 
-    f0 = 0.001  # Source peak frequency is 1Hz (0.001 kHz)
+    f0 = 0.008  # Source peak frequency is 8Hz (0.008 kHz)
     src = RickerSource(name='src', grid=grid, f0=f0,
                        npoint=1, time_range=time_range)
 
     src.coordinates.data[0, 0] = 4800.
-    src.coordinates.data[0, 1] = 2000.
+    src.coordinates.data[0, 1] = 4800.
+    src.coordinates.data[0, 2] = 2750.
 
     # Set up snapshotting
     steps = src.nt
@@ -67,7 +67,8 @@ def run(sdf, s_o, nsnaps):
                             save=nsnaps+1, time_dim=t_sub)
 
     eq = dv.Eq(p.forward,
-               2*p - p.backward + dt**2*c**2*(subs[p.dx2] + subs[p.dy2]))
+               2*p - p.backward
+               + dt**2*c**2*(subs[p.dx2] + subs[p.dy2] + subs[p.dz2]))
 
     eq_save = dv.Eq(psave, p)
 
@@ -82,11 +83,11 @@ def run(sdf, s_o, nsnaps):
 def plot_snaps(psave_data, shift, sdf):
     # FIXME: Hardcoded for four snapshots
     # Plot extent
-    plt_ext = (0., 9600., 0.+shift*30., 5100.+shift*30.)
+    plt_ext = (0., 9600., 0.-shift*30., 5100.-shift*30.)
 
     # Plot surface with SDF contours
     xvals = np.linspace(0., 9600., psave_data.shape[1])
-    yvals = np.linspace(0.+shift*30., 5100.+shift*30., psave_data.shape[2])
+    yvals = np.linspace(0.-shift*30., 5100.-shift*30., psave_data.shape[2])
     xmsh, ymsh = np.meshgrid(xvals, yvals, indexing='ij')
 
     fig, axs = plt.subplots(2, 2, constrained_layout=True, figsize=(9.6, 5.1),
@@ -107,22 +108,22 @@ def plot_snaps(psave_data, shift, sdf):
             axs[j, (i+1) % 2].set_xlabel("Distance (m)")
         if (i+1) % 2 == 0:
             axs[j, (i+1) % 2].set_ylabel("Elevation (m)")
-        axs[j, (i+1) % 2].set_yticks([0., 1000., 2000., 3000., 4000.])
+        axs[j, (i+1) % 2].set_yticks([-1000., 0., 1000., 2000., 3000.])
     plt.show()
 
 
 def load_sdf(file, s_o, shift):
     """Load a signed-distance function from file"""
-    sdf_data = -np.load(append_path(file))
+    sdf_data = np.load(append_path(file))
 
     # Move the surface upwards by grid increments
-    nx, ny = sdf_data.shape
-    fill_vals = np.full((nx, shift), np.amax(sdf_data))
-    sdf_shift = np.concatenate((sdf_data[:, shift:], fill_vals), axis=1)
+    nx, ny, nz = sdf_data.shape
+    fill_vals = np.full((nx, ny, shift), np.amax(sdf_data))
+    sdf_shift = np.concatenate((fill_vals, sdf_data[..., :-shift]), axis=2)
 
     # Set up the grid
     # Note: size currently hardcoded
-    extent = (9600., 5100.)
+    extent = (9600., 9600., 5100.)
     grid = dv.Grid(shape=sdf_shift.shape, extent=extent)
     sdf = dv.Function(name='sdf', grid=grid, space_order=s_o)
     sdf.data[:] = sdf_shift
@@ -136,13 +137,13 @@ def append_path(file):
 
 
 def main():
-    shift = 20  # Number of grid increments to shift surface
+    shift = 50  # Number of grid increments to shift surface
     s_o = 4  # Space order
     # Load the signed distance function data
-    sdf_file = "/../infrasound/surface_files/mt_st_helens_2d.npy"
+    sdf_file = "/../infrasound/surface_files/mt_st_helens_3d.npy"
     sdf = load_sdf(sdf_file, s_o, shift)
 
-    outfile = append_path("/2D_rigid_surface_snaps.npy")
+    outfile = append_path("/3D_free_surface_snaps.npy")
 
     nsnaps = 4  # Number of snaps
 
@@ -150,7 +151,7 @@ def main():
     # If output found then plot it
     try:
         psave_data = np.load(outfile)
-        plot_snaps(psave_data, shift, sdf)
+        plot_snaps(psave_data[:, :, 85], shift, sdf[:, 85])
     except FileNotFoundError:
         psave_data = run(sdf, s_o, nsnaps)
         np.save(outfile, psave_data)
