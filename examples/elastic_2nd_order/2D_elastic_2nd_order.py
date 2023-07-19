b18a559644ee88daf3edb5df984fef009b41812c
EdCaunt <ec815@ic.ac.uk>
2023-07-19 17:57:19 +0100

    examples: added a VTI switch to the 2nd-order elastic demo with Mt St Helens topography

--- examples/elastic_2nd_order/2D_elastic_2nd_order.py
+++ examples/elastic_2nd_order/2D_elastic_2nd_order.py
@@ -18,31 +18,8 @@ from schism import BoundaryGeometry, BoundaryConditions, Boundary
 from examples.seismic import TimeAxis, RickerSource
 
 
-def run(sdf, s_o, nsnaps):
-    """Run a forward model if no file found to read"""
-    grid = sdf.grid
-    bg = BoundaryGeometry(sdf)
-
-    # Create fields
-    ux = dv.TimeFunction(name='ux', grid=grid,
-                         space_order=s_o, time_order=2)
-    uy = dv.TimeFunction(name='uy', grid=grid,
-                         space_order=s_o, time_order=2)
-
-    nx = bg.n[0]
-    ny = bg.n[1]
-
-    v_p = 2.5  # km/s
-    v_s = 1.5  # km/s
-    rho = 3.  # tons/m^3
-
-    # Lame parameters
-    mu = rho*v_s**2
-    lam = rho*v_p**2 - 2*mu
-
-    # For convenience
-    b = 1/rho
-
+def get_iso_bcs(nx, ny, ux, uy, lam, mu, s_o):
+    """Returns boundary conditions for the isotropic case"""
     bc_list = [dv.Eq(nx*(lam+2*mu)*ux.dx + nx*lam*uy.dy
                      + ny*mu*ux.dy + ny*mu*uy.dx, 0),
                dv.Eq(nx*mu*ux.dy + nx*mu*uy.dx
@@ -64,6 +41,143 @@ def run(sdf, s_o, nsnaps):
                              + uy.dxdy2*nx*(lam**2 + 2*lam*mu + 2*mu**2)
                              + uy.dy3*ny*(lam**2 + 4*lam*mu + 4*mu**2), 0))
 
+    return bc_list
+
+
+def get_vti_bcs(nx, ny, ux, uy, v_p2, v_s2, ep, de, s_o):
+    """Returns boundary conditions for the vti case"""
+    # More shorthands
+    v_p4 = v_p2**2
+    v_s4 = v_s2**2
+
+    # Note that a factor of rho has been removed here
+    txx = (1+2*ep)*v_p2*ux.dx + (de*v_p2-v_p2+2*v_s2)*uy.dy
+    tyy = (1+2*ep)*v_p2*uy.dy + (de*v_p2-v_p2+2*v_s2)*ux.dx
+    txy = v_s2*ux.dy + v_s2*uy.dx
+
+    # With fourth-order boundary conditions
+    bc_list = [dv.Eq(nx*txx + ny*txy, 0),
+               dv.Eq(nx*txy + ny*tyy, 0)]
+
+    if s_o >= 4:
+        bc4 = [dv.Eq(ny*ux.dy3*v_s4
+                     + ux.dx2dy*ny*(de*v_p2*np.sqrt(2*de*v_p4
+                                                    - 2*de*v_p2*v_s2
+                                                    + v_p4
+                                                    - 2*v_p2*v_s2
+                                                    + v_s4)
+                                    + 2*ep*v_p2*v_s2 + ny*v_p2*v_s2
+                                    - v_p2*np.sqrt(2*de*v_p4
+                                                   - 2*de*v_p2*v_s2
+                                                   + v_p4 - 2*v_p2*v_s2
+                                                   + v_s4)
+                                    + 2*v_s2*np.sqrt(2*de*v_p4
+                                                     - 2*de*v_p2*v_s2
+                                                     + v_p4
+                                                     - 2*v_p2*v_s2
+                                                     + v_s4))
+                     + ux.dx3*nx*(4*ep**2*v_p4 + 4*ep*v_p4 + v_p4)
+                     + ux.dxdy2*nx*(2*ep*v_p2*v_s2 + v_p2*v_s2
+                                    + v_s2*np.sqrt(2*de*v_p4
+                                                   - 2*de*v_p2*v_s2
+                                                   + v_p4 - 2*v_p2*v_s2
+                                                   + v_s4))
+                     + uy.dx2dy*nx*(2*de*ep*v_p4 + de*v_p4 - 2*ep*v_p4
+                                    + 4*ep*v_p2*v_s2 - v_p4
+                                    + 2*v_p2*v_s2
+                                    + v_s2*np.sqrt(2*de*v_p4
+                                                   - 2*de*v_p2*v_s2
+                                                   + v_p4
+                                                   - 2*v_p2*v_s2
+                                                   + v_s4))
+                     + uy.dx3*ny*(2*ep*v_p2*v_s2 + v_p2*v_s2)
+                     + uy.dxdy2*ny*(2*ep*v_p2*np.sqrt(2*de*v_p4
+                                                      - 2*de*v_p2*v_s2
+                                                      + v_p4
+                                                      - 2*v_p2*v_s2
+                                                      + v_s4)
+                                    + v_p2*np.sqrt(2*de*v_p4
+                                                   - 2*de*v_p2*v_s2
+                                                   + v_p4
+                                                   - 2*v_p2*v_s2
+                                                   + v_s4)
+                                    + v_s4)
+                     + uy.dy3*nx*(de*v_p2*v_s2 - v_p2*v_s2 + 2*v_s4), 0),
+               dv.Eq(nx*ux.dy3*v_p2*v_s2 + nx*uy.dx3*v_s4
+                     + ux.dx2dy*nx*(2*ep*v_p2*np.sqrt(2*de*v_p4
+                                                      - 2*de*v_p2*v_s2
+                                                      + v_p4
+                                                      - 2*v_p2*v_s2
+                                                      + v_s4)
+                                    + v_p2*np.sqrt(2*de*v_p4
+                                                   - 2*de*v_p2*v_s2
+                                                   + v_p4
+                                                   - 2*v_p2*v_s2 + v_s4)
+                                    + v_s4)
+                     + ux.dx3*ny*(de*v_p2*v_s2 - v_p2*v_s2 + 2*v_s4)
+                     + ux.dxdy2*ny*(de*v_p4 - v_p4 + 2*v_p2*v_s2
+                                    + v_s2*np.sqrt(2*de*v_p4
+                                                   - 2*de*v_p2*v_s2
+                                                   + v_p4 - 2*v_p2*v_s2
+                                                   + v_s4))
+                     + uy.dx2dy*ny*(2*ep*v_p2*v_s2 + v_p2*v_s2
+                                    + v_s2*np.sqrt(2*de*v_p4
+                                                   - 2*de*v_p2*v_s2 + v_p4
+                                                   - 2*v_p2*v_s2 + v_s4))
+                     + uy.dxdy2*nx*(de*v_p2*np.sqrt(2*de*v_p4
+                                                    - 2*de*v_p2*v_s2 + v_p4
+                                                    - 2*v_p2*v_s2 + v_s4)
+                                    + v_p2*v_s2
+                                    - nx*v_p2*np.sqrt(2*de*v_p4
+                                                      - 2*de*v_p2*v_s2
+                                                      + v_p4 - 2*v_p2*v_s2
+                                                      + v_s4)
+                                    + 2*v_s2*np.sqrt(2*de*v_p4
+                                                     - 2*de*v_p2*v_s2 + v_p4
+                                                     - 2*v_p2*v_s2 + v_s4))
+                     + uy.dy3*ny*(2*ep*v_p4 + v_p4))]
+
+        bc_list += bc4
+
+    return bc_list
+
+
+def run(sdf, s_o, nsnaps, mode):
+    """Run a forward model if no file found to read"""
+    grid = sdf.grid
+    bg = BoundaryGeometry(sdf)
+
+    # Create fields
+    ux = dv.TimeFunction(name='ux', grid=grid,
+                         space_order=s_o, time_order=2)
+    uy = dv.TimeFunction(name='uy', grid=grid,
+                         space_order=s_o, time_order=2)
+
+    nx = bg.n[0]
+    ny = bg.n[1]
+
+    v_p = 2.5  # km/s
+    v_s = 1.5  # km/s
+    rho = 3.  # tons/m^3
+
+    # For convenience
+    b = 1/rho
+
+    if mode == 'iso':
+        # Lame parameters
+        mu = rho*v_s**2
+        lam = rho*v_p**2 - 2*mu
+        bc_list = get_iso_bcs(nx, ny, ux, uy, lam, mu, s_o)
+    elif mode == 'vti':
+        # Anisotropy parameters
+        de = 0.1
+        ep = 0.25
+
+        v_p2 = v_p**2
+        v_s2 = v_s**2
+        v_pn2 = (1+2*de)*v_p**2
+        bc_list = get_vti_bcs(nx, ny, ux, uy, v_p2, v_s2, ep, de, s_o)
+
     # TODO: add higher-order bcs
     bcs = BoundaryConditions(bc_list)
     boundary = Boundary(bcs, bg)
@@ -76,17 +190,9 @@ def run(sdf, s_o, nsnaps):
     # Note: grid increment hardcoded, courant number 0.5
     dt = 0.5*30/v_p  # Time step from grid spacing
 
-    # Stability check
-    alpha = np.sqrt((lam+2*mu)/rho)
-    beta = np.sqrt(mu/rho)
-    dx = np.amin(grid.spacing)
-    print(dt)
-    print("Must be smaller than")
-    print(dx/np.sqrt(alpha**2 + beta**2))
-
     time_range = TimeAxis(start=t0, stop=tn, step=dt)
 
-    f0 = 0.0065  # Source peak frequency is 6.5Hz (0.0065 kHz)
+    f0 = 0.006  # Source peak frequency is 6Hz (0.006 kHz)
     src = RickerSource(name='src', grid=grid, f0=f0,
                        npoint=1, time_range=time_range)
 
@@ -105,8 +211,14 @@ def run(sdf, s_o, nsnaps):
     uysave = dv.TimeFunction(name='uysave', grid=grid, time_order=0,
                              save=nsnaps+1, time_dim=t_sub)
 
-    rhs_ux = b*(lam+2*mu)*ux.dx2 + b*mu*ux.dy2 + b*(lam+mu)*uy.dxdy
-    rhs_uy = b*(lam+2*mu)*uy.dy2 + b*mu*uy.dx2 + b*(lam+mu)*ux.dxdy
+    if mode == 'iso':
+        rhs_ux = b*(lam+2*mu)*ux.dx2 + b*mu*ux.dy2 + b*(lam+mu)*uy.dxdy
+        rhs_uy = b*(lam+2*mu)*uy.dy2 + b*mu*uy.dx2 + b*(lam+mu)*ux.dxdy
+    elif mode == 'vti':
+        rhs_ux = v_p2*(1+2*ep)*ux.dx2 + v_s2*ux.dy2 \
+            + np.sqrt((v_p2-v_s2)*(v_pn2-v_s2))*uy.dxdy
+        rhs_uy = v_s2*uy.dx2 + v_p2*uy.dy2 \
+            + np.sqrt((v_p2-v_s2)*(v_pn2-v_s2))*ux.dxdy
 
     eq_ux = dv.Eq(ux.forward,
                   2*ux - ux.backward
@@ -128,14 +240,14 @@ def run(sdf, s_o, nsnaps):
     return uxsave.data, uysave.data
 
 
-def plot_snaps(vxsave_data, vysave_data, shift, sdf):
+def plot_snaps(uxsave_data, uysave_data, shift, sdf):
     # FIXME: Hardcoded for four snapshots
     # Plot extent
     plt_ext = (0., 9600., 0.-shift*30., 5100.-shift*30.)
 
     # Plot surface with SDF contours
-    xvals = np.linspace(0., 9600., vxsave_data.shape[1])
-    yvals = np.linspace(0.-shift*30., 5100.-shift*30., vxsave_data.shape[2])
+    xvals = np.linspace(0., 9600., uxsave_data.shape[1])
+    yvals = np.linspace(0.-shift*30., 5100.-shift*30., uxsave_data.shape[2])
     xmsh, ymsh = np.meshgrid(xvals, yvals, indexing='ij')
 
     fig, axs = plt.subplots(4, 2, constrained_layout=True,
@@ -143,9 +255,9 @@ def plot_snaps(vxsave_data, vysave_data, shift, sdf):
                             sharex=True, sharey=True)
 
     for i in range(1, 5):
-        vmax = np.amax(np.abs(vxsave_data[i]))
+        vmax = np.amax(np.abs(uxsave_data[i]))
         vmin = -vmax
-        axs[i-1, 0].imshow(vxsave_data[i].T, origin='lower',
+        axs[i-1, 0].imshow(uxsave_data[i].T, origin='lower',
                            extent=plt_ext, vmax=vmax, vmin=vmin,
                            cmap='seismic')
         axs[i-1, 0].contour(xmsh, ymsh, sdf.data, [0])
@@ -155,9 +267,9 @@ def plot_snaps(vxsave_data, vysave_data, shift, sdf):
         axs[i-1, 0].set_yticks([-1000., 0., 1000., 2000., 3000.])
 
     for i in range(1, 5):
-        vmax = np.amax(np.abs(vysave_data[i]))
+        vmax = np.amax(np.abs(uysave_data[i]))
         vmin = -vmax
-        axs[i-1, 1].imshow(vysave_data[i].T, origin='lower',
+        axs[i-1, 1].imshow(uysave_data[i].T, origin='lower',
                            extent=plt_ext, vmax=vmax, vmin=vmin,
                            cmap='seismic')
         axs[i-1, 1].contour(xmsh, ymsh, sdf.data, [0])
@@ -198,21 +310,26 @@ def main():
     sdf_file = "/../infrasound/surface_files/mt_st_helens_2d.npy"
     sdf = load_sdf(sdf_file, s_o, shift)
 
-    outfile_vx = append_path("/2D_elastic_2nd_order_vx_snaps.npy")
-    outfile_vy = append_path("/2D_elastic_2nd_order_vy_snaps.npy")
+    # Mode, can be 'iso' or 'vti
+    mode = 'vti'
+
+    outfile_ux = append_path("/2D_elastic_2nd_order_ux_snaps_"
+                             + mode + ".npy")
+    outfile_uy = append_path("/2D_elastic_2nd_order_uy_snaps_"
+                             + mode + ".npy")
 
     nsnaps = 4  # Number of snaps
 
     # If not run then run and save output
     # If output found then plot it
     try:
-        vxsave_data = np.load(outfile_vx)
-        vysave_data = np.load(outfile_vy)
-        plot_snaps(vxsave_data, vysave_data, shift, sdf)
+        uxsave_data = np.load(outfile_ux)
+        uysave_data = np.load(outfile_uy)
+        plot_snaps(uxsave_data, uysave_data, shift, sdf)
     except FileNotFoundError:
-        vxsave_data, vysave_data = run(sdf, s_o, nsnaps)
-        np.save(outfile_vx, vxsave_data)
-        np.save(outfile_vy, vysave_data)
+        uxsave_data, uysave_data = run(sdf, s_o, nsnaps, mode)
+        np.save(outfile_ux, uxsave_data)
+        np.save(outfile_uy, uysave_data)
 
 
 if __name__ == "__main__":
